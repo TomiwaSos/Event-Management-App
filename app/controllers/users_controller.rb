cfb72dda9fbfc1e1b0992878c6c29070dcc69dd9
Tomiwa Sosanya <tomiwasosanya@Tomiwas-MacBook-Pro.local>
2023-03-25 19:44:32 +0000

    user form buttons are accurate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "Successfully created user."
+      redirect_to users_path, notice: "Successfully created user."
     else
       render :new, status: :unprocessable_entity
     end
