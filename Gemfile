3c2ee49aa47b2bf8fe77d1494f92dbcf3e11d575
Tomiwa Sosanya <tomiwasosanya@Tomiwas-MacBook-Pro.local>
2023-03-25 19:02:24 +0000

    small update to event active toggle

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem "jbuilder"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 gem "bcrypt", "~> 3.1.7"
 
+# frontend helper
 gem 'bootstrap', '~> 5.2', '>= 5.2.3'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,23 @@
 class Event < ApplicationRecord
   has_many :comments
   belongs_to :user, foreign_key: 'created_by'
+
+  after_save :check_if_active
+
+  def check_if_active
+    today = Date.today
+    if (active_from.to_date..active_to.to_date) === today
+      update_column(:active, true)
+    else
+      update_column(:active, false)
+    end
+  end
+
+  def author
+    User.find(created_by).username
+  end
+
+  def status
+    Status.find(status_id).name
+  end
 end
