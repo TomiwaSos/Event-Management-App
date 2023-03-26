f1bc43408a12d101e7bf4b3502c2c5ba03b54cab
Tomiwa Sosanya <tomiwasosanya@Tomiwas-MacBook-Pro.local>
2023-03-26 20:51:36 +0100

    adjusts migration to suit heroku

--- db/migrate/20230318155649_add_fields_to_user.rb
+++ db/migrate/20230318155649_add_fields_to_user.rb
@@ -2,6 +2,6 @@ class AddFieldsToUser < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :username, :string
     add_column :users, :email, :string
-    add_column :users, :password_digest, :encrypted_password
+    add_column :users, :password_digest, :password
   end
 end
